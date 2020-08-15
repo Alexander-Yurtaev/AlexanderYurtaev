fb98e9a16f0da988fad3590ab152bfcb5cc56ee7
Alexander-Yurtaev <alexander.yurtaev@gmail.com>
2020-08-15 17:39:23 +0200

    TaskExtension was added.

---
+++ AlexanderYurtaev.Framework/Extensions/TaskExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AlexanderYurtaev.Framework.Extensions
+{
+    public static class TaskExtension
+    {
+        public static async void Await(this Task task, Action completed = null, Action<Exception> callbackError = null)
+        {
+            try
+            {
+                await task;
+                completed?.Invoke();
+            }
+            catch (Exception ex)
+            {
+                callbackError?.Invoke(ex);
+            }
+        }
+    }
+}
