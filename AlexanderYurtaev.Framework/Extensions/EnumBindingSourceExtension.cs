e751e9277b7a55ee056879968735208d285a98c8
Alexander-Yurtaev <alexander.yurtaev@gmail.com>
2020-08-17 18:18:52 +0200

    EnumBindingSourceExtension was added.

---
+++ AlexanderYurtaev.Framework/Extensions/EnumBindingSourceExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace AlexanderYurtaev.Framework.Extensions
+{
+    public class EnumBindingSourceExtension : MarkupExtension
+    {
+        public Type EnumType { get; private set; }
+
+        public EnumBindingSourceExtension(Type enumType)
+        {
+            if (enumType == null || enumType.IsEnum)
+                throw new ArgumentException($"{nameof(EnumType)} must not be null and of type {nameof(Enum)}");
+
+            EnumType = enumType;
+        }
+
+        #region Overrides of MarkupExtension
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Enum.GetValues(EnumType);
+        }
+
+        #endregion
+    }
+}
