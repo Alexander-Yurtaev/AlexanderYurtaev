346579528ab156ae415fd1902137caedab26b069
Alexander-Yurtaev <alexander.yurtaev@gmail.com>
2020-08-31 18:54:39 +0200

    LocalizationHelper class was created.

---
+++ AlexanderYurtaev.Framework/LocalizationHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace AlexanderYurtaev.Framework
+{
+    public static class LocalizationHelper
+    {
+        public static void SetGlobalLanguage()
+        {
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.Name)));
+        }
+
+        public static void SetGlobalFlowDirection()
+        {
+            var flowDirection = CultureInfo.CurrentUICulture.TextInfo.IsRightToLeft
+                ? FlowDirection.RightToLeft
+                : FlowDirection.LeftToRight;
+
+            FrameworkElement.FlowDirectionProperty.OverrideMetadata(typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(flowDirection));
+        }
+
+        public static void SetGlobalLocalization()
+        {
+            SetGlobalLanguage();
+            SetGlobalFlowDirection();
+        }
+    }
+}
