9c23e74f037d236065a1c158ba5a8ef6c69a3d63
Alexander-Yurtaev <alexander.yurtaev@gmail.com>
2020-09-02 10:30:58 +0200

    Refactoring.

--- AlexanderYurtaev.Framework/Helpers/LocalizationHelper.cs
+++ AlexanderYurtaev.Framework/Helpers/LocalizationHelper.cs
@@ -1,8 +1,11 @@
-﻿using System.Globalization;
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+using System.Globalization;
 using System.Windows;
 using System.Windows.Markup;
 
-namespace AlexanderYurtaev.Framework
+namespace AlexanderYurtaev.Framework.Helpers
 {
     public static class LocalizationHelper
     {
