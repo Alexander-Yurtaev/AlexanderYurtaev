e5d934098399ade5805da565f041e40fa1fcdad0
Alexander-Yurtaev <alexander.yurtaev@gmail.com>
2020-09-23 13:53:21 +0200

    VisibleableRowDefinition and VisibleableColumnDefinition were added.

---
+++ AlexanderYurtaev.Framework/UI/VisibleableColumnDefinition.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AlexanderYurtaev.Framework.UI
+{
+    public class VisibleableColumnDefinition : ColumnDefinition
+    {
+        static VisibleableColumnDefinition()
+        {
+            WidthProperty.OverrideMetadata(typeof(VisibleableColumnDefinition),
+                new FrameworkPropertyMetadata(new GridLength(1, GridUnitType.Star), null, CoerceWidth));
+        }
+
+        private static object CoerceWidth(DependencyObject d, object value)
+        {
+            return (((VisibleableColumnDefinition)d).Visible) ? value : new GridLength(0);
+        }
+
+        #region Dependency Property Visible
+
+        public static readonly DependencyProperty VisibleProperty = DependencyProperty.Register(
+            "Visible", typeof(bool), typeof(VisibleableColumnDefinition), new PropertyMetadata(true, OnVisibleChanged));
+
+        public bool Visible
+        {
+            get => (bool)GetValue(VisibleProperty);
+            set => SetValue(VisibleProperty, value);
+        }
+
+        private static void OnVisibleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            d.CoerceValue(WidthProperty);
+        }
+
+        #endregion Dependency Property Visible
+    }
+}
---
+++ AlexanderYurtaev.Framework/UI/VisibleableRowDefinition.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AlexanderYurtaev.Framework.UI
+{
+    public class VisibleableRowDefinition : RowDefinition
+    {
+        static VisibleableRowDefinition()
+        {
+            HeightProperty.OverrideMetadata(typeof(VisibleableRowDefinition),
+                new FrameworkPropertyMetadata(new GridLength(1, GridUnitType.Star), null, CoerceHight));
+        }
+
+        private static object CoerceHight(DependencyObject d, object value)
+        {
+            return (((VisibleableRowDefinition)d).Visible) ? value : new GridLength(0);
+        }
+
+        #region Dependency Property Visible
+
+        public static readonly DependencyProperty VisibleProperty = DependencyProperty.Register(
+            "Visible", typeof(bool), typeof(VisibleableRowDefinition), new PropertyMetadata(true, OnVisibleChanged));
+
+        public bool Visible
+        {
+            get => (bool)GetValue(VisibleProperty);
+            set => SetValue(VisibleProperty, value);
+        }
+
+        private static void OnVisibleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            d.CoerceValue(HeightProperty);
+        }
+
+        #endregion Dependency Property Visible
+    }
+}
